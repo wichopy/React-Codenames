de0f5067b3c1ab2fbb689ab9831db75eec93c965
William Chou <will@secretmission.io>
2017-07-04 22:28:58 -0400

    Randomize roles of each cell per game.

--- src/App.js
+++ src/App.js
@@ -3,53 +3,80 @@ import logo from './logo.svg';
 import './App.css';
 import faker from 'faker';
 
-const wordCell = (id, value, selectHandler) => {
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
+const wordCell = (id, value, type, selectHandler) => {
   return ce('td', {
-    className: 'word-cell',
+    className: 'word-cell ' + type,
     name: id,
     onClick: () => selectHandler(id, value),
   }, value);
 };
 
-// const RandomNumber = (min, max) => {
-//   //from https://stackoverflow.com/questions/34182699/random-integer-in-a-certain-range-excluding-one-number
-//   const exclusionsSorted = exclusions.concat().sort(function(a, b) {
-//     return a - b
-//   });
-//   let randomNumber = Math.floor(Math.random() * max) + min;
-//   for(let i = 0; i < max; i++) {
-//     if (exclusionsSorted.findrandomNumber >= exclusionsSorted[i]) {
-//       randomNumber++;
-//     }
-//   }
-//   return randomNumber;
-// }
+const RandomNumber = (min, max) => {
+  let randomNumber = Math.floor(Math.random() * max) + min;
+  return randomNumber;
+}
 
-const initialState = {
-  size: 5, //default 5 x 5 grid
-  gridValues: Array(25).fill({ word: '', type: '' }),
+const initialState = () => {
+  // default 5 x 5 grid
+  const unindexedGridValues = Array(25).fill({ word: '', type: '' });
+  const gridValues = unindexedGridValues.map((cell, index) => { return { ...cell, index } });
+  return {
+    size: 5,
+    gridValues,
+  }
 };
 
-// const setBackgrounds = () => {
-//   const gridValues = this.state.gridValues.concat();
-//   const sessionGameBoard = {};
-//   const populateCount = 0;
-//   //set red team 
-//   for(let i = 0; i < 9; i++){
-//     let newRandomPosition = RandomNumber(0, size * size - 1, exclusionList)
-//     exclusionList.push(newRandomPosition);
-//     sessionGameBoard[newRandomPosition] = { owner: 'Red' };
-//   }
-//   //set blue team
-//   for(let i = 0; i < 9; i++){
-//     let newRandomPosition = RandomNumber(0, size * size - 1, exclusionList)
-//     exclusionList.push(newRandomPosition);
-//     sessionGameBoard[newRandomPosition] = { owner: 'Red' };
-//   }
-// }
+const setBackgrounds = (colorlessGrid, size) => {
+  const gridValues = colorlessGrid.concat();
+  let populateCount = 0;
+  //set red team 
+  for(let i = 0; i <= 8; i++){
+    let newRandomPosition = RandomNumber(0, size * size - 1 - populateCount);
+    gridValues[newRandomPosition].type = 'Red'
+    populateCount++;
+    gridValues.push(gridValues[newRandomPosition]);
+    gridValues.remove(newRandomPosition);
+    console.log('red')
+  }
+  //set blue team
+  for(let i = 0; i <= 7; i++){
+    let newRandomPosition = RandomNumber(0, size * size - 1 - populateCount);
+    gridValues[newRandomPosition].type = 'Blue'
+    populateCount++;
+    gridValues.push(gridValues[newRandomPosition]);
+    gridValues.remove(newRandomPosition);
+    console.log('blue')
+  }
+  // set assassin
+  gridValues[0].type = 'Assassin';
+  populateCount++;
+  gridValues.push(gridValues[0]);
+  gridValues.remove(0);
+  console.log('assassin')
+  // set innocent peoples
+  for(let i = 0; i <= 6; i++){
+    let newRandomPosition = RandomNumber(0, size * size - 1 - populateCount);
+    gridValues[newRandomPosition].type = 'Innocent'
+    populateCount++;
+    gridValues.push(gridValues[newRandomPosition]);
+    gridValues.remove(newRandomPosition);
+    console.log('innocent')
+  }
+  console.log('Done adding colors');
+  console.log('------');
+  console.log(gridValues);
+  return gridValues;
+}
 
 class App extends React.Component {
-  state = initialState;
+  state = initialState();
 
   componentWillMount() {
     const gridValues = [...this.state.gridValues];
@@ -63,6 +90,9 @@ class App extends React.Component {
 
   componentDidMount() {
     console.log('App mounted.')
+    const addBackgrounds = setBackgrounds(this.state.gridValues, this.state.size);
+    const gridValues = addBackgrounds.sort((a,b) => a.index - b.index);
+    this.setState({ gridValues });
   }
 
   gridRows = (size) => {
@@ -75,7 +105,7 @@ class App extends React.Component {
             ce('tr', {},
               this.state.gridValues.slice(i*size, i*size+size).map((cell, index) => {
                 console.log('on cell', i*size+index);
-                return wordCell(i*size+index, cell.word, this.selectWord)
+                return wordCell(i*size+index, cell.word, cell.type, this.selectWord)
               })
             )
           )
@@ -92,11 +122,6 @@ class App extends React.Component {
   render() {
     return (
       ce('div', { className: 'App' },
-        ce('div', { className: 'App-header' },
-          ce('img', { src: logo , className: 'App-logo', alt: 'logo' },
-          ),
-          ce('h2', {}, 'React Codenames'),
-        ),
         ce('div', { className: 'container-fluid' },
           ce('div', { className: 'row'},
             ce('div', { className: 'col-4' }),
