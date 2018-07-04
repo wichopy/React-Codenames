683c662001e370d66202f7e61637c12a2c92d06d
Will C <w.chou06@gmail.com>
2018-07-03 22:40:49 -0400

    Styleds components

--- src/Components/MainMenu.jsx
+++ src/Components/MainMenu.jsx
@@ -1,10 +1,21 @@
 import { createElement as ce } from 'react'
+import styled from 'styled-components'
 
-const MainMenu = ({}) => (
-  ce('div', {},
+const StyledMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`
+
+const MainMenu = ({ onNewGameClick, onGameNameChange }) => (
+  ce(StyledMenu, {},
     ce('h1', {}, 'Start a new game'),
-    ce('input', { type: 'text'},),
-    ce('button', { }, 'Create New Game'),
+    ce('div', {} ,
+      ce('input', { type: 'text', onChange: onGameNameChange },),
+      ce('button', { onClick: onNewGameClick }, 'Create New Game'),
+    )
   )
 )
 
--- stories/index.js
+++ stories/index.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
-import MainMenu from '../src/Components/MainMenu'
+import MainMenu from '../src/Components/MainMenu';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
